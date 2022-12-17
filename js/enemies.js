3cb10bd7a1b62c82ad3bbbec650f6b40de54b0ee
CoreyOliver <coreyoliver.dev@gmail.com>
2022-12-17 16:20:05 -0500

    enemy addition complete

--- js/enemies.js
+++ js/enemies.js
@@ -78,8 +78,26 @@ export class ClimbingEnemy extends Enemy {
         this.x = this.game.width
         this.y = Math.random() * this.game.height * 0.5
         this.image = enemy_spider_big
-        this.speedX =
-        this.speedY =
+        this.speedX = 0
+        this.speedY = Math.random() > 0.5 ? 1 : -1
         this.maxFrame = 5
+    }
+    
+    update(deltaTime) {
+        super.update(deltaTime)
+        if(this.y > this.game.height - this.height - this.game.groundMargin) {
+            this.speedY *= -1
+        }
+        if(this.y < -this.height) {this.markedForDeletion = true}
+
+    }
+
+    draw(context) {
+        super.draw(context)
+        context.beginPath()
+        context.moveTo(this.x + this.width/2,0)
+        context.lineTo(this.x + this.width/2,this.y + 50)
+        context.stroke()
+
     }
 }
