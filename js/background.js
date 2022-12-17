f7de5713714529bc536e3422defeac08a85150e1
CoreyOliver <coreyoliver.dev@gmail.com>
2022-12-16 19:32:31 -0500

    state speed management added

--- js/background.js
+++ js/background.js
@@ -32,10 +32,10 @@ export class Background {
         this.layer3image = layer3
         this.layer4image = layer4
         this.layer5image = layer5
-        this.layer1 = new Layer( this.game, this.width, this.height, 1, this.layer1image)
-        this.layer2 = new Layer( this.game, this.width, this.height, 1, this.layer2image)
-        this.layer3 = new Layer( this.game, this.width, this.height, 1, this.layer3image)
-        this.layer4 = new Layer( this.game, this.width, this.height, 1, this.layer4image)
+        this.layer1 = new Layer( this.game, this.width, this.height, 0, this.layer1image)
+        this.layer2 = new Layer( this.game, this.width, this.height, .2, this.layer2image)
+        this.layer3 = new Layer( this.game, this.width, this.height, .4, this.layer3image)
+        this.layer4 = new Layer( this.game, this.width, this.height, .8, this.layer4image)
         this.layer5 = new Layer( this.game, this.width, this.height, 1, this.layer5image)
         this.backgroundLayers = [this.layer1, this.layer2, this.layer3, this.layer4, this.layer5]
     }
